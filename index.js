46f46d3cd5e91699c9e6d8bc3e3364d3728c38a2
kelsieszost <owner@kelsieszost.design>
2022-10-18 16:20:40 -0400

    database creation

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 const inquirer = require('inquirer');
 const consoleTable = ('console.table');
 const mysql = require('mysql');
+
+var db = mysql.createConnection(
+    { 
+    host: '127.0.0.1',
+    port: '3005',
+    datbase: 'employees_db',
+    user: 'root',
+    password: 'newyork22',
+    }
+);
+
+db.connect((err) => {
+    if (err) {
+        console.log('Connection Failed');
+    } else {
+        console.log('Employee Manager Main Menu');
+        firstsprompt
+    }
+}
+)
